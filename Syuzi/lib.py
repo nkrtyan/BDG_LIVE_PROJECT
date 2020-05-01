a21e9fa7c9442b148421aad4c3077d56c38cdb6e
Suzanna23 <tgsuzannadavtyan@gmail.com>
2020-05-02 00:35:57 +0400

    Add folders

---
+++ Syuzi/lib.py
@@ -0,0 +1,28 @@
+class LIB:
+    #create chrome driver
+    def open_browser(self):
+    
+    #navigate to given element
+    def page_load(self,browser):
+
+    
+    #open txt file and write there text
+    def write_to_file(self,text):
+
+    #move to element
+    def move_to_element(self,browser,element):
+
+    #wait for element
+    def wait_for_element(self,browser,element):
+
+
+    #data parsing
+    def get_data(self_key):
+
+    #save screenshot
+    def save_screenshot(self,browser:)
+
+    #close browser
+    def close_browser(self):
+
+        
