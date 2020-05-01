66ea21c8b57150ea314feeaff8398acd11bf0e20
kkhudaverdyan <arkstell!123>
2020-05-01 15:05:53 -0700

    3 POMs and Lib.py are created

---
+++ Kristina Kh/POM/Sign_in.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+class Sign_in:
+    # locators
+    email_address    = (By.ID, 'email')
+    password         = (By.ID, 'password') 
+    sign_in_btn      = (By.ID, 'SubmitLogin') 
+    my_account_title = (By.ID, "//a[contains(text(),'My account')]") 
+
+    # init
+    def __init__(self, browser):
+        self.browser  = browser
