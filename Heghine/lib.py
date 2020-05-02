51463ec1cec1694f5020a8800c16b43a812beed2
Heghine93 <hexine.tsaturyan@gmail.com>
2020-05-02 11:47:48 +0400

    updated

--- Heghine/lib.py
+++ Heghine/lib.py
@@ -58,7 +58,7 @@ class LIB:
 
     #wait for given element to be visible in UI
     def wait_for_element(self, browser, element):
-        try:
+        try: 
             WebDriverWait(browser, 100).until(EC.visibility_of_all_element_located(element))
         except:
             print("element is not visible")
@@ -83,7 +83,7 @@ class LIB:
     def save_screenshot(self, browser):
         current_filename=os.path.basename(sys.argv[0][:-3])
         try:
-            browser.save_screenshot(f'Test\{current_filename}_screenshot.png')
+            browser.save_screenshot(f'Test\\{current_filename}_screenshot.png')
         except:
             print("screenshot is not saved")
 
