daf12fa09b459b29701f0beae7399a1a9fc79f02
Heghine93 <hexine.tsaturyan@gmail.com>
2020-05-03 18:36:15 +0400

    updated

--- Heghine/POM/Home.py
+++ Heghine/POM/Home.py
@@ -6,7 +6,7 @@ class Home_page:
     # Locators 
     page_logo      = (By.XPATH,'//*[@id="header_logo"]/a/img')
     contact_us     = (By.XPATH,'//*[@id="contact-link"]/a')
-    sign_in        = (By.XPATH,'//*[@id="contact-link"]/a')
+    sign_in        =(By.XPATH,"//*[@id='header']//a[@class='login']")    
     women          = (By.XPATH,'//*[@id="block_top_menu"]/ul/li[1]/a')
     dresses        = (By.XPATH,'//*[@id="block_top_menu"]/ul/li[2]/a')
     t_shirts       = (By.XPATH,'//*[@id="block_top_menu"]/ul/li[3]/a')
